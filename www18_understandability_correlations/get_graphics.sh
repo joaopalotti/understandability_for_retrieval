b3bb5e24b6cbbdf23507904a85a28ba0960e24b1
joaopalotti <joaopalotti@gmail.com>
2017-10-22 00:13:12 +0300

    Updated figures and text in section 6.

---
+++ www18_understandability_correlations/get_graphics.sh
@@ -0,0 +1,15 @@
+cp /home/palotti/github/clef_readability/chapter7/box_kendalltau16_raw_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/box_pearson16_raw_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/box_spearman16_raw_values.pdf graphics/.
+#
+cp /home/palotti/github/clef_readability/chapter7/box_kendalltau15_raw_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/box_pearson15_raw_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/box_spearman15_raw_values.pdf graphics/.
+#
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_pearson15_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_spearman15_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_kendalltau15_values.pdf graphics/.
+#
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_pearson16_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_spearman16_values.pdf graphics/.
+cp /home/palotti/github/clef_readability/chapter7/bar_corr_kendalltau16_values.pdf graphics/.
