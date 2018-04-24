65823b1980ca921666d1187c005a9b2c09bad74d
joaopalotti <joaopalotti@gmail.com>
2018-04-24 10:49:56 +0300

    Added data from www appendix paper

---
+++ www18_understandability_correlations/appendix/get_graphics.sh
@@ -0,0 +1,24 @@
+cp ~/github/clef_readability/appendix/unders_per_topic_clef16.png .
+cp ~/github/clef_readability/appendix/hist_unders.png hist_unders16.png
+cp ~/github/clef_readability/chapter7/bar_raw_values.png .
+
+cp ~/github/clef_readability/chapter7/bar_corr_spearman15_values.png .
+cp ~/github/clef_readability/chapter7/bar_corr_pearson15_values.png .
+cp ~/github/clef_readability/chapter7/bar_corr_kendalltau15_values.png .
+
+cp ~/github/clef_readability/chapter7/bar_corr_spearman16_values.png .
+cp ~/github/clef_readability/chapter7/bar_corr_pearson16_values.png .
+cp ~/github/clef_readability/chapter7/bar_corr_kendalltau16_values.png .
+
+cp /home/palotti/github/clef_readability/chapter7/regressors_corr.png .
+
+
+cp ~/github/clef_readability/chapter7/box_spearman15_raw_values.png .
+cp ~/github/clef_readability/chapter7/box_kendalltau15_raw_values.png .
+cp ~/github/clef_readability/chapter7/box_pearson15_raw_values.png .
+
+cp ~/github/clef_readability/chapter7/box_spearman16_raw_values.png .
+cp ~/github/clef_readability/chapter7/box_kendalltau16_raw_values.png .
+cp ~/github/clef_readability/chapter7/box_pearson16_raw_values.png .
+
+cp ~/github/clef_readability/chapter7/legendCorr.png .
